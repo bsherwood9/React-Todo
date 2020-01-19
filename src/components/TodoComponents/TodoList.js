effbb3b65c4decb98c1ecc0a136bf72b5a8abc24
Brett Sherwood <36565536+bsherwood9@users.noreply.github.com>
2020-01-18 17:41:56 -0700

    Revert "Brett sherwood"

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,24 +1,2 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
-import React from "react";
-
-import Todo from "./Todo";
-const TodoList = props => {
-  if (!props.searchTerm) {
-    return props.todoList.map(item => {
-      if (item.task) {
-        return <Todo item={item} key={item.id} toggleTodo={props.toggleTodo} />;
-      }
-    });
-  } else {
-    return props.todoList
-      .filter(item =>
-        item.task.toLowerCase().includes(props.searchTerm.toLowerCase())
-      )
-      .map(item => (
-        <Todo item={item} key={item.id} toggleTodo={props.toggleTodo} />
-      ));
-  }
-};
-
-export default TodoList;
